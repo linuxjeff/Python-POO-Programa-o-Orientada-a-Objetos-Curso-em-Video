d43bb400e8a8ddab1a2fc43b6b8388d1a6ff37b0
jefferson Santana <computadores.rp@gmail.com>
2026-09-24 18:43:47 -0300

    Aula 02

---
+++ Aula-02.py
@@ -0,0 +1,10 @@
+"""Nesta aula, você vai entender de forma clara e prática por que a Programação
+ Orientada a Objetos é tão importante no desenvolvimento moderno. Falamos sobre
+  organização de código, reaproveitamento, manutenção, escalabilidade e como a
+   POO prepara você para projetos maiores e para o mercado de trabalho."""
+
+# 006 - D Reutilizavel
+# 007 - B Extensível
+# 008 - A Oportuno
+# 009 - A Natural
+# 010 - C Confiável
