ea9e71d678871d9c0f6dc5f65e02ebef373689a3
jefferson Santana <computadores.rp@gmail.com>
2026-09-26 09:55:17 -0300

    Aula 03

---
+++ aula 03.py
@@ -0,0 +1,62 @@
+""" Nesta aula fundamental do curso de Python, mergulhamos nos conceitos
+ essenciais da Orientação a Objetos (POO). Você vai aprender, de forma simples
+ e didática, o que são classes, objetos, atributos e métodos, utilizando
+ analogias práticas como fôrmas de biscoitos e plantas de casas. Entenda como
+ esses conceitos se aplicam não apenas ao Python, mas a diversas linguagens
+ modernas como Java, PHP e C++. """
+
+# O que é um objeto?
+#"Coisa material ou abstrata que é feita a partie de uma classe e pode ser
+# descrita por meio das sues atributos, metodos e estado atual."
+
+# O que é uma classe?
+# Classe serve para criar padrões para objetos.
+# Nome da class
+# Atributo: Características que tenho
+# Métodos: Coisas que posso fazer
+
+# Quando acontece um instaciamento?
+# É o ato de criar um objeto a partir da classe.
+
+
+# Sabe definir o estado de um objeto?
+# O estado é as variações de todos os atributos num dado momento.
+
+# 011 C Notificação
+# 012 A Instaciar
+# 013 B Métodos
+# 014 D Estado
+
+""" 015
+Concreto
+     Fio de cobre
+         atributos
+             Peso
+             Cor da capa
+             Diâmetro
+         Métodos
+            + Conetctado()
+            + Encapado()
+            + Enrolado()
+    Estado do fio
+        Peso: 8 Quilos
+        Cor da capa: Amarela
+        Diâmetro: 12mm
+Abstrato
+    Piquenique
+        Atributos
+            Horário
+            Tipo de alimentos
+            Tipo de utensílios
+            Local
+        Métodos
+            + Iniciar()
+            + Alimenta-se()
+            + Conversar()
+            + Descansar()
+    Estado
+        Horário: das 8 às 11
+        Tipo de alimentos: Somente frios
+        Tipo de utensílios: Cestas e descartáveis
+        Local: Parque da Cidade
+ """
